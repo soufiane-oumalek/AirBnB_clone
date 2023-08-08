6e452735b7357e678ea4c72d0fd2b5c63a6d946b
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-08-08 21:07:43 +0000

    creat method

--- models/base_model.py
+++ models/base_model.py
@@ -21,3 +21,25 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.now().isoformat()
         self.updated_at = datetime.now().isoformat()
+    
+    def __str__(self):
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+    
+    def save(self):
+        self.updated_at = datetime.now().isoformat()
+    
+    def to_dict(self)
+    dictionary = {}
+    
+
+        
+
+
+
+soufiane = BaseModel()
+soufiane.name = "soufiane"
+soufiane.my_number = 93956
+print(soufiane)
+print(type(soufiane.id))
+print(soufiane.created_at)
+print(soufiane.updated_at)
