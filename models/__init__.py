97b3f7107e259cfdb0292e6ecdc13a68b720d6e3
omarIdhmaid <o.idhmaid@gmail.com>
2023-08-12 12:26:35 +0100

    void

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-make my directory an module
-"""
+"""make my directory an module"""
 from .engine.file_storage import FileStorage
 
 
--- models/base_model.py
+++ models/base_model.py
@@ -1,58 +1,26 @@
 #!/usr/bin/python3
-"""
-base model of our airBnB
-"""
+"""base model of our airBnB"""
+
 from datetime import datetime
 import models
 from uuid import uuid4
 
 
 class BaseModel:
-    """
-    Define BaseModel class
-    """
+    """Define BaseModel class"""
 
     def __init__(self, *args, **kwargs):
-        """
-        Initializes the BaseModel object.
-        """
-        if kwargs:
-            for key in kwargs:
-                match key:
-                    case "__class__":
-                        pass
-                    case "id":
-                        self.id = kwargs[key]
-                    case "created_at":
-                        self.created_at = datetime.strptime(kwargs[key], "\
-%Y-%m-%dT%H:%M:%S.%f")
-                    case "updated_at":
-                        self.updated_at = datetime.strptime(kwargs[key], "\
-%Y-%m-%dT%H:%M:%S.%f")
-                    case _:
-                        setattr(self, key, kwargs[key])
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        models.storage.new(self)
+        """Initializes the BaseModel object."""
+        pass
 
     def __str__(self):
         """string method that return descriptor of the object"""
-        return ("[{}] ({}) {}\
-".format(self.__class__.__name__, self.id, self.__dict__))
+        pass
 
     def save(self):
         """save method that save time update and update"""
-        self.updated_at = datetime.now()
-        models.storage.save()
+        pass
 
     def to_dict(self):
         """to_dict method that get an prepared dict"""
-        dictionary = {}
-        for key in self.__dict__:
-            dictionary[key] = self.__dict__[key]
-        dictionary["__class__"] = self.__class__.__name__
-        dictionary["created_at"] = self.created_at.isoformat()
-        dictionary["updated_at"] = self.updated_at.isoformat()
-        return dictionary
+        pass
