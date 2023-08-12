eed294e397ce09b4e64bf76be9759f58f2693be1
omarIdhmaid <o.idhmaid@gmail.com>
2023-08-12 17:20:27 +0100

    debagging

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" unit test for bases """
+import json
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+import models
+from io import StringIO
+import sys
+from unittest.mock import patch
+captured_output = StringIO()
+sys.stdout = captured_output
+
+
+class BaseModelTestCase(unittest.TestCase):
+    """ class for base test """
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
