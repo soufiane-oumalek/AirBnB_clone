5c998e238c78fe49cfb4fff573cd4e617eb5164e
omarIdhmaid <o.idhmaid@gmail.com>
2023-08-12 18:02:17 +0100

    fix user str casting

--- models/base_model.py
+++ models/base_model.py
@@ -32,13 +32,13 @@ class BaseModel:
                     case "__class__":
                         pass
                     case "id":
-                        self.id = kwargs[key]
+                        setattr(self, key, str(kwargs[key]))
                     case "created_at":
-                        self.created_at = datetime.strptime(kwargs[key], "\
-%Y-%m-%dT%H:%M:%S.%f")
+                        setattr(self, key, datetime.strptime(kwargs[key], "\
+%Y-%m-%dT%H:%M:%S.%f"))
                     case "updated_at":
-                        self.updated_at = datetime.strptime(kwargs[key], "\
-%Y-%m-%dT%H:%M:%S.%f")
+                        setattr(self, key, datetime.strptime(kwargs[key], "\
+%Y-%m-%dT%H:%M:%S.%f"))
                     case _:
                         setattr(self, key, kwargs[key])
         else:
@@ -70,15 +70,10 @@ class BaseModel:
         Return:
             new dictionary
         """
-        # dictionary = {}
-        # for key in self.__dict__:
-        #     dictionary[key] = self.__dict__[key]
-        # dictionary["__class__"] = self.__class__.__name__
-        # dictionary["created_at"] = self.created_at.isoformat()
-        # dictionary["updated_at"] = self.updated_at.isoformat()
-        # return dictionary
-        new_dict = self.__dict__.copy()
-        new_dict["__class__"] = self.__class__.__name__
-        new_dict["created_at"] = self.created_at.isoformat()
-        new_dict["updated_at"] = self.updated_at.isoformat()
-        return new_dict
+        dictionary = {}
+        for key in self.__dict__:
+            dictionary[key] = self.__dict__[key]
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
