d3b44cb3fe03d8a83b0e6ef7f5d7522361c93a57
omarIdhmaid <o.idhmaid@gmail.com>
2023-08-13 12:28:38 +0100

    check inside the file JSON

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -47,6 +47,33 @@ class FileStorageTest(unittest.TestCase):
             self.assertTrue(key.split(".\
 ")[1] == storage.all()[key].to_dict()["id"])
 
+    def test_save(self):
+        b = BaseModel()
+        u = User()
+        s = State()
+        p = Place()
+        c = City()
+        a = Amenity()
+        r = Review()
+        storage.new(b)
+        storage.new(u)
+        storage.new(s)
+        storage.new(p)
+        storage.new(c)
+        storage.new(a)
+        storage.new(r)
+        storage.save()
+        save_text = ""
+        with open("file.json", "r") as f:
+            save_text = f.read()
+            self.assertIn("BaseModel." + b.id, save_text)
+            self.assertIn("User." + u.id, save_text)
+            self.assertIn("State." + s.id, save_text)
+            self.assertIn("Place." + p.id, save_text)
+            self.assertIn("City." + c.id, save_text)
+            self.assertIn("Amenity." + a.id, save_text)
+            self.assertIn("Review." + r.id, save_text)
+
 
 if __name__ == '__main__':
     unittest.main()
