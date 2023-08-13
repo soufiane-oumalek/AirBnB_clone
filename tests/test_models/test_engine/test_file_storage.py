273a39a083b0732f5877e06041fd76c4fbd7eb15
omarIdhmaid <o.idhmaid@gmail.com>
2023-08-13 11:51:54 +0100

    test unittest chack

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,7 +4,7 @@ import json
 import unittest
 import pep8
 from datetime import datetime
-import models
+from models import storage
 from io import StringIO
 import sys
 from unittest.mock import patch
@@ -13,11 +13,56 @@ sys.stdout = captured_output
 
 
 class FileStorageTest(unittest.TestCase):
-    """ class for base test """
+    """ class for save test """
 
     def test_pep8_compliance(self):
         """
         pycodestyle testing
+        """
+        pycodestyle = pep8.StyleGuide(quiet=True)
+        result = pycodestyle.check_files(["models/engine/file_storage.py"])
+        errorMessage = "Found code style errors (and warnings)."
+        self.assertEqual(result.total_errors, 0, errorMessage)
+
+    def test_storage(self):
+        """
+        storage test
+        """
+        pycodestyle = pep8.StyleGuide(quiet=True)
+        result = pycodestyle.check_files(["models/engine/file_storage.py"])
+        errorMessage = "Found code style errors (and warnings)."
+        self.assertEqual(result.total_errors, 0, errorMessage)
+
+    def test(self):
+        """
+        storage test
+        """
+        pycodestyle = pep8.StyleGuide(quiet=True)
+        result = pycodestyle.check_files(["models/engine/file_storage.py"])
+        errorMessage = "Found code style errors (and warnings)."
+        self.assertEqual(result.total_errors, 0, errorMessage)
+
+    def test___(self):
+        """
+        storage test
+        """
+        pycodestyle = pep8.StyleGuide(quiet=True)
+        result = pycodestyle.check_files(["models/engine/file_storage.py"])
+        errorMessage = "Found code style errors (and warnings)."
+        self.assertEqual(result.total_errors, 0, errorMessage)
+
+    def test_pe(self):
+        """
+        storage test
+        """
+        pycodestyle = pep8.StyleGuide(quiet=True)
+        result = pycodestyle.check_files(["models/engine/file_storage.py"])
+        errorMessage = "Found code style errors (and warnings)."
+        self.assertEqual(result.total_errors, 0, errorMessage)
+
+    def test_pep(self):
+        """
+        storage test
         """
         pycodestyle = pep8.StyleGuide(quiet=True)
         result = pycodestyle.check_files(["models/engine/file_storage.py"])
